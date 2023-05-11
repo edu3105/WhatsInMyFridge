8df6aa9dc6270f5dc7a8b60943b779f1b4948459
edu3105 <eugeniusadmission@gmail.com>
2023-05-11 15:32:08 +0800

    commit for react native base library ui

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { NativeBaseProvider, Box } from "native-base";
+
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import HomeScreen from './page/Homescreen';
-import SettingsScreen from './page/settings';
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './page/HomeScreen.js';
+import SettingsScreen from './page/Settings.js';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,27 +15,39 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list' : 'ios-list-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
+        screenOptions={{
+          tabBarStyle: {
+            height: 60, // Set the height of the tab bar here
           },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
+        }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} 
+          options={{ 
+            tabBarBadge: 5,
+            tabBarLabel: ' ',
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'home' : 'home-outline'}
+                size={30} // Set the size of the icon here
+                focused={focused}
+                marginBottom={-15}
+              />
+            ),
+          }} 
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen}
+          options={{ 
+            tabBarLabel: ' ',
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? 'settings' : 'settings-outline'}
+                size={30} // Set the size of the icon here
+                focused={focused}
+                marginBottom={-15}
+              />
+            ),
+          }} 
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
