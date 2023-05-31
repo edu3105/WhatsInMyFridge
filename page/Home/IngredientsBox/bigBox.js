89c7b8b4ab5518c85588bc6eb253bedac37d8581
110006223 <samuel.sukatja@gmail.com>
2023-05-31 20:02:43 +0800

    5/31 home updated version, adding modal and fixing components for further usability

--- page/Home/IngredientsBox/bigBox.js
+++ page/Home/IngredientsBox/bigBox.js
@@ -1,33 +1,60 @@
-import {React, useState} from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { 
-  NativeBaseProvider, 
-  Box, 
-  AspectRatio, 
-  Image, 
-  Center, 
-  Stack, 
-  Heading, 
-  HStack, 
-  ScrollView, 
-  Input, 
-  Pressable, 
+import { React, useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  NativeBaseProvider,
+  Box,
+  AspectRatio,
+  Image,
+  Center,
+  Stack,
+  Heading,
+  HStack,
+  ScrollView,
+  Input,
+  Pressable,
   Button,
-  Flex
+  Flex,
 } from "native-base";
-import Ingredients from './ingredients.js';
-import AddButton from './addButton.js';
+import Ingredients from "./ingredients.js";
+import AddButton from "./addButton.js";
 
 const bigBox = () => {
-    return (
-      <NativeBaseProvider>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Ingredients />
-        </View>
+  const { height } = Dimensions.get("window");
 
-        <AddButton />
-      </NativeBaseProvider>
-    );
-  };
-  
-  export default bigBox;
+  return (
+    <NativeBaseProvider>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ScrollView
+          nestedScrollEnabled={true}
+          vertical={true}
+          style={{ flexDirection: "column" }}
+          bg="coolGray.300"
+          mx="5"
+          mt="3"
+          w="80%"
+          h={height * 0.5}
+        >
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+          <Ingredients />
+        </ScrollView>
+      </View>
+
+      <AddButton />
+    </NativeBaseProvider>
+  );
+};
+
+export default bigBox;
