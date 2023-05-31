f6872f71d4fb9b2a3a081d3962820cc6f6ec61aa
jason <jonartojason@gmail.com>
2023-05-31 19:47:14 +0800

    Added new things bro

--- page/Explore/ExploreBoxes.js
+++ page/Explore/ExploreBoxes.js
@@ -1,141 +1,176 @@
-import {React, useState} from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { 
-    NativeBaseProvider, 
-    Box, 
-    AspectRatio, 
-    Image, 
-    Center, 
-    Stack, 
-    Heading, 
-    HStack, 
-    ScrollView, 
-    Input, 
-    Pressable, 
-    Button,
-    TouchableOpacity,
-    Avatar,
-    Flex
-  } from "native-base";
+import { React, useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  NativeBaseProvider,
+  Box,
+  AspectRatio,
+  Image,
+  Center,
+  Stack,
+  Heading,
+  HStack,
+  ScrollView,
+  Input,
+  Container,
+  Pressable,
+  Button,
+  TouchableOpacity,
+  Avatar,
+  Flex,
+  VStack,
+} from "native-base";
 
 export default function MainBoxes() {
-    const [isClicked, setIsClicked] = useState(false);
-    return (
-        <NativeBaseProvider>
-          <Heading size="md" ml="10"> For You</Heading>
-
-            <Box flex={1} height="100%"alignItems="center" py={10}>
-              <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700"
-              }} _web={{
-                shadow: 2,
-                borderWidth: 0
-              }} _light={{
-                backgroundColor: "gray.50"
-              }}>
-                <Box>
-                  <AspectRatio w="100%" ratio={16 / 9}>
-                    <Image source={{
-                    uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
-                    }} alt="image" />
-                  </AspectRatio>
-                  <Center bg="violet.500" _dark={{
-                    bg: "violet.400"
-                  }} _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs"
-                  }} position="absolute" bottom="0" px="3" py="1.5">
-                      PHOTOS
-                  </Center>
-                </Box>
-                <Stack p="4" space={3}>
-                  <Stack space={2}>
-                    <Heading size="md" ml="-1">
-                      The Garden City
-                    </Heading>
-                    <Text fontSize="xs" _light={{
-                    color: "violet.500"
-                  }} _dark={{
-                    color: "violet.400"
-                  }} fontWeight="500" ml="-0.5" mt="-1">
-                      The Silicon Valley of India.
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <NativeBaseProvider>
+      <Heading size="md" ml="10">
+        {" "}
+        For You
+      </Heading>
+      <Text>ㅤ</Text>
+      <Center>
+        <Box
+          maxW="400"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="coolGray.200"
+          borderWidth="1"
+          _dark={{
+            borderColor: "coolGray.600",
+            backgroundColor: "gray.700",
+          }}
+          _web={{
+            shadow: 2,
+            borderWidth: 0,
+          }}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+        >
+          <HStack space={3}>
+            <Center>
+              <HStack space={1}>
+                <Text>ㅤ</Text>
+                <Image
+                  source={{
+                    uri: "https://media.istockphoto.com/id/477596753/photo/homemade-southern-fried-chicken.jpg?s=612x612&w=0&k=20&c=8b_5KjKmmO34ChsqVZN4-1YlDXyVJNlKh3w6qtVjR88=",
+                  }}
+                  alt="Alternate Text"
+                  size="lg"
+                />
+              </HStack>
+            </Center>
+            <VStack space={1}>
+              <Heading size="md" ml="-1">
+                AYAM KFC
+              </Heading>
+              <VStack>
+                <Text>Fried chicken, also known as</Text>
+                <Text>Southern fried chicken, is a dish</Text>
+                <Text>consisting of chicken pieces that</Text>
+                <Text>have been coated with seasoned</Text>
+                <Text>flour or batter and pan-fried chickenㅤ</Text>
+                <Text>while retaining.....</Text>
+              </VStack>
+              <VStack>
+                <HStack
+                  alignItems="flex-end"
+                  space={4}
+                  justifyContent="flex-end"
+                >
+                  <HStack alignItems="flex-end">
+                    <Text
+                      color="coolGray.600"
+                      _dark={{
+                        color: "warmGray.200",
+                      }}
+                      fontWeight="400"
+                    >
+                      ㅤTap to see moreㅤ
                     </Text>
-                  </Stack>
-                  <Text fontWeight="400">
-                    Bengaluru (also called Bangalore) is the center of India's high-tech
-                    industry. The city is also known for its parks and nightlife.
-                  </Text>
-                  <HStack alignItems="center" space={4} justifyContent="space-between">
-                    <HStack alignItems="center">
-                      <Text color="coolGray.600" _dark={{
-                      color: "warmGray.200"
-                    }} fontWeight="400">
-                        6 mins ago
-                      </Text>
-                    </HStack>
                   </HStack>
-                </Stack>
-              </Box>
-            </Box>  
+                </HStack>
+                <Text>ㅤ</Text>
+              </VStack>
+            </VStack>
+          </HStack>
+        </Box>
+      </Center>
 
-            <Heading size="md" ml="10"> Trending now</Heading>
-            <Box flex={1} height="100%"alignItems="center" py={10}>
-              <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700"
-              }} _web={{
-                shadow: 2,
-                borderWidth: 0
-              }} _light={{
-                backgroundColor: "gray.50"
-              }}>
-                <Box>
-                  <AspectRatio w="100%" ratio={16 / 9}>
-                    <Image source={{
-                    uri: "https://avatars.githubusercontent.com/u/61780838?v=4"
-                    }} alt="image" />
-                  </AspectRatio>
-                  <Center bg="violet.500" _dark={{
-                    bg: "violet.400"
-                  }} _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs"
-                  }} position="absolute" bottom="0" px="3" py="1.5">
-                      PHOTOS
-                  </Center>
-                </Box>
-                <Stack p="4" space={3}>
-                  <Stack space={2}>
-                    <Heading size="md" ml="-1">
-                      The Garden City
-                    </Heading>
-                    <Text fontSize="xs" _light={{
-                    color: "violet.500"
-                  }} _dark={{
-                    color: "violet.400"
-                  }} fontWeight="500" ml="-0.5" mt="-1">
-                      The Silicon Valley of India.
+      <Heading size="md" ml="10">
+        {" "}
+        Trending now
+      </Heading>
+      <Text>ㅤ</Text>
+      <Center>
+        <Box
+          maxW="400"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="coolGray.200"
+          borderWidth="1"
+          _dark={{
+            borderColor: "coolGray.600",
+            backgroundColor: "gray.700",
+          }}
+          _web={{
+            shadow: 2,
+            borderWidth: 0,
+          }}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+        >
+          <HStack space={3}>
+            <Center>
+              <HStack space={1}>
+                <Text>ㅤ</Text>
+                <Image
+                  source={{
+                    uri: "https://media.istockphoto.com/id/477596753/photo/homemade-southern-fried-chicken.jpg?s=612x612&w=0&k=20&c=8b_5KjKmmO34ChsqVZN4-1YlDXyVJNlKh3w6qtVjR88=",
+                  }}
+                  alt="Alternate Text"
+                  size="lg"
+                />
+              </HStack>
+            </Center>
+            <VStack space={1}>
+              <Heading size="md" ml="-1">
+                AYAM KFC
+              </Heading>
+              <VStack>
+                <Text>Fried chicken, also known as</Text>
+                <Text>Southern fried chicken, is a dish</Text>
+                <Text>consisting of chicken pieces that</Text>
+                <Text>have been coated with seasoned</Text>
+                <Text>flour or batter and pan-fried chickenㅤ</Text>
+                <Text>while retaining.....</Text>
+              </VStack>
+              <VStack>
+                <HStack
+                  alignItems="flex-end"
+                  space={4}
+                  justifyContent="flex-end"
+                >
+                  <HStack alignItems="flex-end">
+                    <Text
+                      color="coolGray.600"
+                      _dark={{
+                        color: "warmGray.200",
+                      }}
+                      fontWeight="400"
+                    >
+                      ㅤTap to see moreㅤ
                     </Text>
-                  </Stack>
-                  <Text fontWeight="400">
-                    Bengaluru (also called Bangalore) is the center of India's high-tech
-                    industry. The city is also known for its parks and nightlife.
-                  </Text>
-                  <HStack alignItems="center" space={4} justifyContent="space-between">
-                    <HStack alignItems="center">
-                      <Text color="coolGray.600" _dark={{
-                      color: "warmGray.200"
-                    }} fontWeight="400">
-                        6 mins ago
-                      </Text>
-                    </HStack>
                   </HStack>
-                </Stack>
-              </Box>
-            </Box>
-        </NativeBaseProvider>
-    )
+                </HStack>
+                <Text>ㅤ</Text>
+              </VStack>
+            </VStack>
+          </HStack>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
+  );
 }
