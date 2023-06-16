33b3760df34ba085f0502d921309893fb04e1f10
s1mply <hsuowen7@gmail.com>
2023-06-16 15:38:05 +0800

    Box properly deleted

--- page/Bookmark/BookmarkScreen.js
+++ page/Bookmark/BookmarkScreen.js
@@ -4,35 +4,30 @@ import { Box, Card, NativeBaseProvider, Stack, Center, Divider, Button } from "n
 
 import { fetchRecipeById } from "../api/api.js";
 
-/*
-Right now the box will disappear in both stacks, probably because the index of both the boxes are the same.
-fix with uuid later on. thanks. -Owen 6/15
-
-test is this changed?
-
-*/
 
 
 export default function BookmarkScreen({ navigation }) {
   const [oddStack, setOddStack] = useState([]);
   const [evenStack, setEvenStack] = useState([]);
   const [recipeCount, setRecipeCount] = useState(1);
+  const [uid, setUId] = useState(9238);
 
   function addComponent() {
     var isOdd = recipeCount % 2 === 1 ? true : false;
 
     if (isOdd) {
-      setOddStack([...oddStack, recipeCount]);
+      setOddStack([...oddStack, {recipeCount, uid}]);
     } else {
-      setEvenStack([...evenStack, recipeCount]);
+      setEvenStack([...evenStack, {recipeCount, uid}]);
     }
+    setUId(uid + 1);
     setRecipeCount(recipeCount + 1);
   }
 
-  function removeComponent(index) {
-    var newStack = oddStack.filter((_, i) => i !== index);
+  function removeComponent(id) {
+    var newStack = oddStack.filter((el) => el.uid != id);
     setOddStack(newStack);
-    newStack = evenStack.filter((_, i) => i !== index);
+    newStack = evenStack.filter((el) => el.uid != id);
     setEvenStack(newStack);
   }
 
@@ -41,10 +36,11 @@ export default function BookmarkScreen({ navigation }) {
       <ScrollView>
         <Stack direction="row" justifyContent="space-evenly" top="5">
           <Stack direction="column" space="md">
-            {oddStack.map((recipeId, index) => newBox(recipeId, index))}
+            {oddStack.map((el)=> newBox(el.recipeCount, el.uid))}
           </Stack>
           <Stack direction="column" space="md">
-            {evenStack.map((recipeId, index) => newBox(recipeId, index))}
+            {/* {evenStack.map((recipeId, uid) => newBox(recipeId, uid))} */}
+            {evenStack.map((el) => newBox(el.recipeCount, el.uid))}
           </Stack>
         </Stack>
         <Button onTouchStart={addComponent}> Create a Box </Button>
@@ -53,15 +49,15 @@ export default function BookmarkScreen({ navigation }) {
   )
 
 
-  function newBox(recipeId, index) {
+  function newBox(recipeId, uid) {
     return (
       <Box
-        key={index}
+        key={uid}
         _text={styles.recipePage.text}
         bg={styles.recipePage.bg}
         height={styles.recipePage.height}
         width={styles.recipePage.width}
-        onTouchStart={() => removeComponent(index)}>
+        onTouchStart={() => removeComponent(uid)}>
         {recipeId}
       </Box>
     );
